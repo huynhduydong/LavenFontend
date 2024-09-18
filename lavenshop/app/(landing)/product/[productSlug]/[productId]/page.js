c28987fd0337c9d799ba4328e18b029e62c19b42
huynhduydong <hddongqn@gmail.com>
2024-09-18 22:50:07 +0700

    feat: update ui product detail

--- lavenshop/app/(landing)/product/[productSlug]/[productId]/page.js
+++ lavenshop/app/(landing)/product/[productSlug]/[productId]/page.js
@@ -7,11 +7,20 @@ import { ProductInformation } from "@/components/custom/ProductDetail/ProductInf
 import { useState, useEffect } from "react";
 import { getProductById } from "@/services/productServices";
 import { useParams } from "next/navigation";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 const Page = () => {
   const [product, setProduct] = new useState({});
   const params = useParams();
-
+  const [isFullDescription, setIsFullDescription] = useState(false);
   console.log(">>> check router:", params);
   const getProductData = async () => {
     const productData = await getProductById(params.productId);
@@ -28,15 +37,65 @@ const Page = () => {
     <>
       {product && product.category && (
         <div className="bg-gray-100 px-32">
-          <BreadCrumb category={product.category} product={product} />
-          <div className="flex p-0 bg-white gap-8">
-            <section className="shrink-0 p-4 block" style={{ width: "450px" }}>
-              <ProductImage product={product} />
-              <ProductSharing />
-            </section>
-            <section className="flex flex-auto">
-              <ProductInformation product={product} />
-            </section>
+          <BreadCrumb category={product.category} information={product.name} />
+          <div className="space-y-4">
+            <div className="flex p-0 bg-white gap-8 rounded-md">
+              <section
+                className="shrink-0 p-4 block"
+                style={{ width: "450px" }}
+              >
+                <ProductImage product={product} />
+              </section>
+              <section className="flex flex-auto">
+                <ProductInformation product={product} />
+              </section>
+            </div>
+
+            <div className="px-4 pt-6 pb-2 bg-white rounded-md space-y-2">
+              <div className="text-lg font-medium ml-4">Thông tin chi tiết</div>
+              <Table>
+                <TableBody>
+                  {product &&
+                    product?.specifications.map((specification) => (
+                      <TableRow key={specification.id}>
+                        <TableCell className="text-gray-500">
+                          {specification.name}
+                        </TableCell>
+                        <TableCell>{specification.value}</TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </div>
+
+            <div className="px-4 pt-6 pb-4 bg-white rounded-md space-y-2">
+              <div className="text-lg font-medium ml-4">Mô tả sản phẩm</div>
+              {product && product?.description && (
+                <div className="flex flex-col justify-center items-center">
+                  <p
+                    className={isFullDescription ? "ml-4" : "ml-4 line-clamp-3"}
+                  >
+                    {product.description.split("\n").map((str, index) => (
+                      <p key={`string-${index}`}>{str}</p>
+                    ))}
+                  </p>
+                  <Button
+                    variant="link"
+                    className={isFullDescription ? "inline" : "hidden"}
+                    onClick={() => setIsFullDescription(false)}
+                  >
+                    Xem ít
+                  </Button>
+                  <Button
+                    variant="link"
+                    className={!isFullDescription ? "inline" : "hidden"}
+                    onClick={() => setIsFullDescription(true)}
+                  >
+                    Xem thêm
+                  </Button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       )}
